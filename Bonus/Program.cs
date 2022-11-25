f4d436746ffbd4cb13e83dd89f0cb830c30cffc0
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-25 16:03:15 +0100

    completato bonus

---
+++ Bonus/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Bonus;
+
+Rettangolo rettangoloPrimo = new Rettangolo();
+Rettangolo rettangoloSecondo = new Rettangolo();
+Rettangolo rettangoloTerzo = new Rettangolo();
+
+rettangoloPrimo.baseRettangolo = 4;
+rettangoloPrimo.altezzaRettangolo = 2;
+
+rettangoloSecondo.baseRettangolo = 6;
+rettangoloSecondo.altezzaRettangolo = 2;
+
+rettangoloTerzo.baseRettangolo = 8;
+rettangoloTerzo.altezzaRettangolo = 4;
+
+Console.WriteLine("-- Rettangolo1 --");
+rettangoloPrimo.StampaRettangolo();
+
+Console.WriteLine();
+// DISEGNO
+
+rettangoloPrimo.Disegna();
+
+Console.WriteLine();
+
+Console.WriteLine("-- Rettangolo2 --");
+rettangoloSecondo.StampaRettangolo();
+
+Console.WriteLine();
+// DISEGNO
+
+rettangoloSecondo.Disegna();
+
+Console.WriteLine();
+
+Console.WriteLine("-- Rettangolo3 --");
+rettangoloTerzo.StampaRettangolo();
+
+Console.WriteLine();
+// DISEGNO
+
+rettangoloTerzo.Disegna();
+
+
---
+++ Bonus/Rettangolo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus
+{
+    public class Rettangolo
+    {
+        // CARATTERISTICHE
+
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        // METODI
+
+        public int CalcoloArea()
+        {
+            int areaRettangolo = baseRettangolo * altezzaRettangolo;
+            return areaRettangolo;
+        }
+
+        public int CalcoloPerimetro()
+        {
+            int perimetroRettangolo = (baseRettangolo + altezzaRettangolo) * 2;
+            return perimetroRettangolo;
+        }
+
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("BASE: \t \t" + baseRettangolo + " cm");
+            Console.WriteLine("ALTEZZA: \t" + altezzaRettangolo + " cm");
+            Console.WriteLine("AREA: \t \t" + CalcoloArea() + " cm2");
+            Console.WriteLine("PERIMETRO: \t" + CalcoloPerimetro() + " cm");
+        }
+
+        public void Disegna()
+        {
+            string disegno = " ";
+
+            for (int i = 0; i<baseRettangolo; i++)
+            {
+                disegno = disegno + "--";
+            }
+
+            disegno = disegno + "\n";
+
+            for (int i = 0; i<altezzaRettangolo; i++)
+            {
+                disegno = disegno + "|";
+                for (int y = 0; y<baseRettangolo; y++)
+                {
+                    disegno = disegno + "  ";
+                }
+                disegno = disegno + "|\n";
+            }
+
+            disegno = disegno + " ";
+
+            for (int i = 0; i < baseRettangolo; i++)
+            {
+                disegno = disegno + "--";
+            }
+
+            Console.WriteLine(disegno);
+        }
+    }
+}
