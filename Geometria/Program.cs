343faf8438cde7fdf203f94010526c127d5fcd55
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-25 15:00:13 +0100

    completato rettangolo con imput

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -2,6 +2,61 @@
 
 using Geometria;
 
+Rettangolo usuario = new Rettangolo();
+
+Console.WriteLine("Inserire dimensione base rettangolo:");
+int baseUsuario = int.Parse(Console.ReadLine());
+usuario.baseRettangolo = baseUsuario;
+
+Console.WriteLine("Inserire dimensione altezza rettangolo:");
+int altezzaUsuario = int.Parse(Console.ReadLine());
+usuario.altezzaRettangolo = altezzaUsuario;
+
+//
+/*
+Console.WriteLine();
+
+Console.WriteLine("Base: " + usuario.baseRettangolo);
+Console.WriteLine("Altezza: " + usuario.altezzaRettangolo);
+*/
+
+Console.WriteLine();
+
+Console.WriteLine("Area: " + usuario.CalcoloArea());
+Console.WriteLine("Perimetro: " + usuario.CalcoloPerimetro());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Per sapere se funzionava il codice
+/*
 Rettangolo rettangoloPrimo = new Rettangolo();
 Rettangolo rettangoloSecondo= new Rettangolo();
 
@@ -26,3 +81,5 @@ Console.WriteLine("L'altezza del 2° rettangolo è: " + rettangoloSecondo.altezz
 
 Console.WriteLine("L'area del 2° rettangolo è: " + rettangoloSecondo.CalcoloArea());
 Console.WriteLine("Il perimetro del 2° rettangolo è: " + rettangoloSecondo.CalcoloPerimetro());
+*/
+
